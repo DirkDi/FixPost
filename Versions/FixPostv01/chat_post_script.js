0dd94798a2911662707cf8d185a039a027a17870
Lukas Niehus <lniehus@live.de>
2020-01-23 17:52:24 +0100

    added patchlogs and tutor pages

---
+++ Versions/FixPostv01/chat_post_script.js
@@ -0,0 +1,11 @@
+// Post script
+/* When send button pressed */
+send_and_upload = document.getElementsByClassName("bt send");
+send_and_upload[0].onclick = function() { extendChat() };
+
+// Upload button
+send_and_upload[1].onclick = function() { /* Let him choose a file */ };
+
+/* Adds an eventlistener who is connected to a function to accept enter button as send button */
+text_field = document.getElementById("chat");
+text_field.addEventListener("keydown", sendByEnter);
